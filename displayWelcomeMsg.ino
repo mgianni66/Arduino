ae88737191d02b3106c1e86e33e128e324d2b4e6
mgianni66 <46239261+mgianni66@users.noreply.github.com>
2018-12-29 09:28:05 +0100

    Add files via upload

---
+++ displayWelcomeMsg.ino
@@ -0,0 +1,33 @@
+void displayWelcomeMsg(int period) {
+
+/* -- display of welcoming message -- */
+
+  lcd.clear();  
+
+  // fixed message
+  char* messages[] = {
+
+  //"01234567890123456789"     Position of display characters  
+    "  TERMOREGOLAZIONE  ",    // line0
+    "   CABINA ARMADIO   ",    // line1
+    "                    ",    // line2
+    " Ver.0.9 27/12/2018 ",    // line3
+  };
+
+ 
+  
+  for(int i = 0; i < 4; i++){
+    lcd.setCursor(0,i);
+    lcd.print(messages[i]);
+  }
+
+  // rolling message
+  char rollMessage = '-';
+  
+  for(int i = 0; i < 20; i++){
+    lcd.setCursor(i,2);
+    lcd.print(rollMessage);
+    delay(period/20);
+  }
+
+}
